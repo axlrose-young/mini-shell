36448da0d2e6b2c8b007c0f8bbb0df48c08ff628
axlrose-young <arjkris789@gmail.com>
2026-04-29 13:45:06 +0530

    Added exit and cd commands

--- main.c
+++ main.c
@@ -3,12 +3,13 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <stdlib.h>
 
 int main(){
 	while(true){
 		char *buffer = getlogin();
-		//buffer = getlogin();
 		printf("[%s]$ ",buffer);
+		
 		//getting input
 		char commands[50];
 		fgets(commands, sizeof(commands), stdin);
@@ -30,7 +31,53 @@ int main(){
 			}
 		}
 		argv[index] = NULL;
+		
+		//getting sizeof argv[]
+		int count = 0;	
+		for(int i = 0; argv[i] != NULL; i++){
+			count++;
+		}
 
+		//getting home dir
+		char *home;
+		home = getenv("HOME");
+		if(home == NULL){
+			printf("Could not get home dir\n");
+		}
+
+		//cd function
+		if(strcmp(argv[0],"cd") == 0){	
+			if(count == 1){
+				chdir(home);
+			}else if(count == 3){
+				printf("cd: too many arguments\n");
+			}else{
+				puts("changing dirs...");
+				chdir(argv[1]);
+			}
+		}
+		
+		//exit function
+		if(strcmp(argv[0],"exit") == 0){
+			if(count > 2){
+				puts("Too many arguments");
+			}else if(count == 2){
+				char *end;
+				int n = strtol(argv[1],&end, 10);
+				if(end == argv[1]){
+					puts("Enter integer value");
+				}else if(*end != '\0'){
+					puts("Enter integer value");
+				}else{
+					exit(n);
+				}
+			}else{
+				exit(0);
+			}
+		}
+
+
+		//forking and executing
 		int pid;
 		pid = fork();
 		if(pid == -1){
@@ -45,3 +92,5 @@ int main(){
 	}
 	return 0;	
 }
+
+
