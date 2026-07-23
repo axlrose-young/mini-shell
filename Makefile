42eb3644f81df2d3204fea2dd50556c685fc468e
axlrose-young <arjkris789@gmail.com>
2026-07-23 18:33:20 +0000

    handles single command

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 all:
-	gcc -Wall -Wextra -o shell main.c tokenizer.c builtin.c executor.c
+	gcc -Wall -Wextra -o shell main.c tokenizer.c executor.c 
 clean:
 	gcc -o shell main.c tokenizer.c builtin.c executor.c
--- executor.c
+++ executor.c
@@ -21,92 +21,102 @@ void apply_redir(Pipes* p){
 */
 
 void exec_commands(Pipes* p, size_t ncmds){
-	//if(ncmds == 1){
-		
-	//}
-	//else{
-	int old_read;	// var to save old read fd
+	if(ncmds == 1){
+		pid_t pid = fork();
+		if(pid == -1){
+			perror("fork error");
+			exit(1);	
+		}		
+		if(pid == 0){
+			execvp(p->cmds[0], p->cmds);
+			perror("exec error");
+			exit(1);
+		}
+		wait(NULL);
+	}
+	else{
+		int old_read;	// var to save old read fd
 
-	for(int i = 0; i < (int)ncmds; i++){
-		if(i == 0){ 			// first command 
-			int pipefd[2];
-			
-			if(pipe(pipefd)	== -1){
-				perror("pipe error");
-				exit(1);	
-			}
-
-			pid_t pid = fork();
-			if(pid == -1){
-				perror("fork error");
-				exit(1);	
-			}	
-
-			if(pid == 0){
-			// child process 	
-				dup2(pipefd[1], STDOUT_FILENO);
-				close(pipefd[0]);
-				close(pipefd[1]);
+		for(int i = 0; i < (int)ncmds; i++){
+			if(i == 0){ 			// first command 
+				int pipefd[2];
 				
-				execvp(p[i].cmds[0], p[i].cmds);
-				perror("exec failed");
-				exit(1);	
-			}
-			// parent process
+				if(pipe(pipefd)	== -1){
+					perror("pipe error");
+					exit(1);	
+				}
 
-			close(pipefd[1]);
-			old_read = pipefd[0];
-			wait(NULL);
-		}	
-		else if(i == (int)(ncmds - 1)){	// last command 
-			
-			pid_t pid = fork();
-			if(pid == -1){
-				perror("fork error");
-				exit(1);	
-			}		
+				pid_t pid = fork();
+				if(pid == -1){
+					perror("fork error");
+					exit(1);	
+				}	
 
-			if(pid == 0){
-				dup2(old_read, STDIN_FILENO);	
-				close(old_read);
+				if(pid == 0){
+				// child process 	
+					dup2(pipefd[1], STDOUT_FILENO);
+					close(pipefd[0]);
+					close(pipefd[1]);
+					
+					execvp(p[i].cmds[0], p[i].cmds);
+					perror("exec failed");
+					exit(1);	
+				}
+				// parent process
 
-				execvp(p[i].cmds[0], p[i].cmds);
-				perror("exec failed");
-				exit(1);	
-			}
-			close(old_read);
-			wait(NULL);
-		}
-		else{				// commands in between 
-			int pipefd[2];
-			
-			if(pipe(pipefd)	== -1){
-				perror("pipe error");
-				exit(1);	
-			}
-
-			pid_t pid = fork();
-			if(pid == -1){
-				perror("fork error");
-				exit(1);	
-			}		
-
-			if(pid == 0){
-				dup2(old_read, STDIN_FILENO);
-				dup2(pipefd[1], STDOUT_FILENO);			
-				close(pipefd[0]);
 				close(pipefd[1]);
-				close(old_read);
+				old_read = pipefd[0];
+				wait(NULL);
+			}	
+			else if(i == (int)(ncmds - 1)){	// last command 
+				
+				pid_t pid = fork();
+				if(pid == -1){
+					perror("fork error");
+					exit(1);	
+				}		
 
-				execvp(p[i].cmds[0], p[i].cmds);
-				perror("exec failed");
-				exit(1);
+				if(pid == 0){
+					dup2(old_read, STDIN_FILENO);	
+					close(old_read);
+
+					execvp(p[i].cmds[0], p[i].cmds);
+					perror("exec failed");
+					exit(1);	
+				}
+				close(old_read);
+				wait(NULL);
 			}
-			close(old_read);	
-			close(pipefd[1]);
-			old_read = pipefd[0];
-			wait(NULL);
-		}
-	}	
-	//}
+			else{				// commands in between 
+				int pipefd[2];
+				
+				if(pipe(pipefd)	== -1){
+					perror("pipe error");
+					exit(1);	
+				}
+
+				pid_t pid = fork();
+				if(pid == -1){
+					perror("fork error");
+					exit(1);	
+				}		
+
+				if(pid == 0){
+					dup2(old_read, STDIN_FILENO);
+					dup2(pipefd[1], STDOUT_FILENO);			
+					close(pipefd[0]);
+					close(pipefd[1]);
+					close(old_read);
+
+					execvp(p[i].cmds[0], p[i].cmds);
+					perror("exec failed");
+					exit(1);
+				}
+				close(old_read);	
+				close(pipefd[1]);
+				old_read = pipefd[0];
+				wait(NULL);
+			}
+		}	
+	}
 }
